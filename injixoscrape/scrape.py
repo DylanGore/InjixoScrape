6e50e631fa908859820273bc950ce2361168b54c
Dylan Gore <hello@dylangore.ie>
2018-08-17 12:42:53 +0100

    Information is now displayed from database rather than processing the HTML each time

--- injixoscrape/scrape.py
+++ injixoscrape/scrape.py
@@ -43,26 +43,27 @@ soup_dict = {}
 
 
 def main(argv):
+    # Print agent username
+    print('\nAgent: ' + agent_username)
     # If the 'new' argument is used when calling the script, delete all downloaded HTML
-    if 'new' in argv and path.exists('html'):
-        rmtree('html')
+    if 'new' in argv:
+        if path.exists('html'):
+            rmtree('html')
+        cleanDatabase()
+        # Pull data from HTML for upcoming events
+        try:
+            loginAndScrape()
+            dashboard_soup = makeSoup(dashboard_file_path)
+            processUpcomingEvents(dashboard_soup)
+            for item in soup_dict:
+                processSevenDaySchedule(item, soup_dict[item])
+        except Exception as e:
+            print('Error reading schedule. Are you sure your login credentials are correct?')
+            print(e)
+            exit(1)
 
-    # Get the parsed HTML code
-    loginAndScrape()
-    dashboard_soup = makeSoup(dashboard_file_path)
-
-    # Cleanup Database
-    cleanDatabase()
-
-    # Pull data from HTML for upcoming events
-    try:
-        processUpcomingEvents(dashboard_soup)
-        for item in soup_dict:
-            processSevenDaySchedule(item, soup_dict[item])
-    except Exception as e:
-        print('Error reading schedule. Are you sure your login credentials are correct?')
-        print(e)
-        exit(1)
+    displayUpcomingEvents()
+    displaySchedule()
 
 
 # Function to handle getting the raw HTML from Injixo
@@ -80,6 +81,7 @@ def loginAndScrape():
     if not path.exists(dashboard_file_path):
         options = webdriver.ChromeOptions()
         options.add_argument('headless')
+        options.add_argument('log-level=3')
         driver = webdriver.Chrome(chrome_options=options)
         driver.get(login_url)
         email_field = driver.find_element_by_id('email')
@@ -140,6 +142,29 @@ def makeCalendarSoup(file_path):
     return cal_soup
 
 
+def displayUpcomingEvents():
+    print('\nUpcoming Events')
+    print('Total Events: ' + str(db_events.count()) + '\n')
+    for event in db_events.find():
+        if event['isMain'] == True:
+            print(event['date'] + ' ' + event['time'] + ', ' + event['name'] + ' [Main Event]')
+        else:
+            print(event['date'] + ' ' + event['time'] + ', ' + event['name'])
+
+
+def displaySchedule():
+    print('\nSchedule:')
+    print('Days: ' + str(db_schedule.count()))
+    for day in db_schedule.find():
+        print('\n' + day['date'])
+        events = day['events']
+        if len(events) != 0:
+            for event in events:
+                print(' ' + event['title'] + ' ' +  event['start_time'] + ' - ' + event['end_time'] + ' (' + event['type'] + ')')
+        else:
+            print(' No events')
+
+
 def processUpcomingEvents(page_soup):
     # Finds and prints the main event from the upcoming events section
     main_event = page_soup.find('div', {'id': 'upcoming-list-next'})
@@ -153,13 +178,7 @@ def processUpcomingEvents(page_soup):
     # Separate date and time into different variables
     main_event_meta = main_event_meta.replace('\n', ' ').replace('\r', '')
     main_event_date = main_event_meta[:13]
-    main_event_time = main_event_meta[15:]
-
-    # Print Title
-    print('\nUpcoming Events:')
-
-    # Print results to console
-    print(main_event_date + ' ' + main_event_time + ' ' + main_event_title + ' [Main Event]')
+    main_event_time = main_event_meta[16:]
 
     # Collects data and inserts into database
     this_event = {'name': main_event_title, 'date': main_event_date, 'time': main_event_time, 'isMain': True,
@@ -195,9 +214,6 @@ def processUpcomingEvents(page_soup):
         # Find event time
         time = upcoming_events_times[i].text
 
-        # Print result to console
-        print(date + ' ' + time + ' ' + title)
-
         # Collects data and inserts into database
         this_event = {'name': title, 'date': date, 'time': time, 'isMain': False, 'agent': agent_username}
         db_events.insert_one(this_event)
@@ -207,14 +223,12 @@ def processSevenDaySchedule(date, page_soup):
     this_events = []
     events = page_soup.findAll('div', {'class': 'fc-content'})
 
-    print(date)
     for event in events:
         event_time = event.find('div', {'class': 'fc-time'}).text
         event_time_start = event_time[:5]
         event_time_end = event_time[8:]
 
         event_name = event.find('div', {'class': 'fc-title'}).text
-        print(event_time_start + ' - ' + event_time_end + ' ' + event_name)
 
         this_event = {'title': event_name, 'start_time': event_time_start, 'end_time': event_time_end,
                       'length': getEventLength(event_time_start, event_time_end),
@@ -222,6 +236,7 @@ def processSevenDaySchedule(date, page_soup):
         this_events.append(this_event)
 
     this_schedule = {
+        'agent': agent_username,
         'date': date,
         'events': this_events
     }
